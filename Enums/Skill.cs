76d6e2018e020f16ded1da04211e77e73012b86f
Hassan K <hassan_99@live.dk>
2023-04-18 11:07:45 +0200

    skill enum with all the skills of the game

---
+++ Enums/Skill.cs
@@ -0,0 +1,36 @@
+﻿namespace rstracker.Enums
+{
+    public enum Skill
+    {
+        TOTAL,
+        ATTACK,
+        DEFENCE,
+        STRENGTH,
+        HITPOINTS,
+        RANGED,
+        PRAYER,
+        MAGIC,
+        COOKING,
+        WOODCUTTING,
+        FLETCHING,
+        FISHING,
+        FIREMAKING,
+        CRAFTING,
+        SMITHING,
+        MINING,
+        HERBLORE,
+        AGILITY,
+        THIEVING,
+        SLAYER,
+        FARMING,
+        RUNECRAFTING,
+        HUNTER,
+        CONSTRUCTION,
+        SUMMONING,
+        DUNGEONEERING,
+        DIVINATION,
+        INVENTION,
+        ARCHAEOLOGY
+    }
+
+}
