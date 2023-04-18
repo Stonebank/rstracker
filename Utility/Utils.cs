9b795ebe7a1f724e6f56dea94e62432ddfda909a
Hassan K <hassan_99@live.dk>
2023-04-18 11:39:42 +0200

    added utility class for easier access of utility operations

---
+++ Utility/Utils.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace rstracker.Utility
+{
+    public class Utils
+    {
+
+        public static string Capitalize(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            string[] words = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                string firstChar = words[i][0].ToString().ToUpper();
+                string restOfChars = words[i][1..];
+                words[i] = $"{firstChar}{restOfChars}";
+            }
+
+            return string.Join(' ', words);
+        }
+
+        public static void PrintLine(string input)
+        {
+            if (input is null)
+                return;
+            Debug.WriteLine($"[{DateTime.Now}]: {input}");
+        }
+
+    }
+}
