56c0c9ffb7673ec5516dd1408cff851ac0536ef4
Hassan K <hassan_99@live.dk>
2023-04-18 11:06:06 +0200

    added constants to maintain global variables

---
+++ Constants.cs
@@ -0,0 +1,15 @@
+﻿namespace rstracker
+{
+    public class Constants
+    {
+
+        /* 
+         * GLOBAL_TRACKING_DELAY will store the time in milliseconds that a player can be tracked.
+         *
+         * The current delay is 60 seconds. This is to prevent the backend from being overloaded with requests.
+         */
+
+        public static long GLOBAL_TRACKING_DELAY = 60 * 1000;
+
+    }
+}
