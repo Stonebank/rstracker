a7e9a44cf3c240fbbe87281662b1a2ecb91055af
Hassan K <hassan_99@live.dk>
2023-04-18 12:16:32 +0200

    fixed usings issue

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,6 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using rstracker.Models;
+using System.Diagnostics;
 
 namespace rstracker.Controllers;
 
