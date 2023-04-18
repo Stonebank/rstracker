e68f51928206e92ed0524722c093797c6cae0f68
Hassan K <hassan_99@live.dk>
2023-04-18 12:16:09 +0200

    added highscore controller to fetch player data

---
+++ Controllers/HighscoreController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using rstracker.Core;
+using System.Net;
+
+namespace rstracker.Controllers
+{
+    public class HighscoreController : Controller
+    {
+
+        [HttpPost]
+        public IActionResult RS3(string username)
+        {
+
+            if (string.IsNullOrEmpty(username))
+                return BadRequest("The given input is either empty or null.");
+
+            var player = DataHandler.GetPlayer(username);
+            if (player is null)
+                return BadRequest($"The player \"{username}\" was not found. This could be because their profile is private or the username input is incorrect.");
+
+            long lastUpdate = player.LastUpdate - DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            if (lastUpdate > 0 && player.SkillData.Count > 0)
+                return View("rs3/profile", player);
+
+            if (DateTime.Today.Date > player.LastResetDay.Date)
+            {
+                player.LastResetDay = DateTime.Today;
+                // clear daily tracking list
+            }
+
+            if (!Constants.PLAYERS.Contains(player))
+                Constants.PLAYERS.Add(player);
+
+            player.SearchCount++;
+
+            if (player.LastSkillDataUpdate.Count > 0)
+                player.LastSkillDataUpdate.Clear();
+
+            string url = $"https://secure.runescape.com/m=hiscore/index_lite.ws?player={username}";
+
+            using (var client = new HttpClient())
+            {
+                HttpResponseMessage response = client.GetAsync(url).GetAwaiter().GetResult();
+                if (!response.IsSuccessStatusCode && response.StatusCode == HttpStatusCode.NotFound)
+                    return BadRequest($"The player \"{username}\" was not found. This could be because their profile is private or the username input is incorrect.");
+                
+                string data = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+                string[] split = data.Split("\n");
+                foreach (var item in split)
+                {
+                    string[] parts = item.Split(',');
+                    if (parts.Length != 3)
+                        continue;
+                    player.SkillData.Add(item);
+                }
+            }
+
+            player.LastUpdate = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() + 60000;
+
+            DataHandler.Save(player);
+
+            return View("rs3/profile", player);
+        }
+
+    }
+}
