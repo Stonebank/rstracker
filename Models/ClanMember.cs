7904a7bd81685d55697b026c943f14d65823e0d4
Hassan K <hassan_99@live.dk>
2023-04-18 17:56:12 +0200

    added clan member model

---
+++ Models/ClanMember.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using rstracker.Utility;
+
+namespace rstracker.Models
+{
+    public class ClanMember
+    {
+
+        public bool IsSuffix { get; set; }
+
+        public bool Recruiting { get; set; }
+
+        public string Name { get; set; }
+
+        public string Clan { get; set; }
+
+        public string Title { get; set; }
+
+        public string World { get; set; }
+
+        public bool Online { get; set; }
+
+        public ClanMember(bool isSuffix, bool recruiting, string name, string clan, string title, string world, bool online)
+        {
+            IsSuffix = isSuffix;
+            Recruiting = recruiting;
+            Name = name;
+            Clan = clan;
+            Title = title;
+            World = world;
+            Online = online;
+        }
+
+        public static string ExtractJsonData(string jsonpResponse)
+        {
+            int startIndex = jsonpResponse.IndexOf('{');
+            int endIndex = jsonpResponse.LastIndexOf('}');
+            if (startIndex >= 0 && endIndex >= 0)
+                return jsonpResponse.Substring(startIndex, endIndex - startIndex + 1);
+            return string.Empty;
+        }
+    }
+}
