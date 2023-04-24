e220be13f9210f3f5a7bc3e9397aa75b988b599e
Hassan K <hassan_99@live.dk>
2023-04-24 22:02:55 +0200

    added cookies and an enum to store and access cookie values

---
+++ Core/CookieHandler.cs
@@ -0,0 +1,35 @@
+﻿using rstracker.Enums;
+
+namespace rstracker.Core
+{
+    public class CookieHandler
+    {
+
+        private static readonly Dictionary<CookieKeys, string> _keys = new Dictionary<CookieKeys, string>();
+
+        public static void Init()
+        {
+            foreach (CookieKeys key in Enum.GetValues(typeof(CookieKeys)))
+                _keys.Add(key, key.ToString().ToLower());
+        }
+
+        public static void SetCookie(HttpResponse response, CookieKeys cookiekey, string value, int? expireDays = null)
+        {
+            var cookieOptions = new CookieOptions
+            {
+                Expires = expireDays.HasValue ? DateTime.Now.AddDays(expireDays.Value) : DateTime.Now.AddDays(365),
+                HttpOnly = true,
+                SameSite = SameSiteMode.Strict,
+                Secure = true
+            };
+
+            response.Cookies.Append(GetCookie(cookiekey), value, cookieOptions);
+        }
+
+        public static string GetCookie(CookieKeys cookiekey)
+        {
+            return _keys[cookiekey];
+        }
+
+    }
+}
---
+++ Enums/CookieKeys.cs
@@ -0,0 +1,15 @@
+﻿namespace rstracker.Enums
+{
+    public enum CookieKeys
+    {
+
+        /* This cookie is used to store the consent of the user to the use of cookies. */
+
+        COOKIE_CONSENT,
+
+        /* Recent searches will store a list of the last 5 players that the user has searched for. */
+
+        RECENT_SEARCHES
+
+    }
+}
