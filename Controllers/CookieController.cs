0241f402b23c18581644890870a4dd4baf8a8a4f
Hassan K <hassan_99@live.dk>
2023-04-25 10:12:12 +0200

    added cookie controlle to handle cookies

---
+++ Controllers/CookieController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using rstracker.Core;
+
+namespace rstracker.Controllers
+{
+    public class CookieController : Controller
+    {
+
+        [HttpPost]
+        public IActionResult SetCookieConsent()
+        {
+            CookieHandler.SetCookie(Response, Enums.CookieKeys.COOKIE_CONSENT, "true");
+            return Ok();
+        }
+
+    }
+}
